81f46ae52c5e46897446b69ac8a3b77c4677f315
Shrey-1123 <you@example.com>
2023-06-21 17:09:47 +0530

    added api decorator

--- force-app/main/default/lwc/helloWebComponent/helloWebComponent.js
+++ force-app/main/default/lwc/helloWebComponent/helloWebComponent.js
@@ -1,3 +1,5 @@
-import { LightningElement } from 'lwc';
+import { LightningElement,api } from 'lwc';
 
-export default class HelloWebComponent extends LightningElement {}
+export default class HelloWebComponent extends LightningElement {
+
+}
